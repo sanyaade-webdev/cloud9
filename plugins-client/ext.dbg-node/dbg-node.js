6018222f9af07e8f010a4ba2882f48796d608b14
nightwing <amirjanyan@gmail.com>
2012-08-26 22:46:58 +0400

    debugger shouldn't continue when reloading page after stepInto

--- plugins-client/ext.dbg-node/dbg-node.js
+++ plugins-client/ext.dbg-node/dbg-node.js
@@ -120,9 +120,13 @@ var v8DebugClient = exports.v8DebugClient = function() {
         if (bp.number != 1)
             return;
 
-        var uibp = mdlDbgBreakpoints.queryNode("//breakpoint[@line='" +
-            frame.getAttribute("line") +"' and @path='" +
-            frame.getAttribute("scriptPath") + "']");
+        var uibp = mdlDbgBreakpoints.queryNode(
+            "//breakpoint[@line='" + frame.getAttribute("line") +"' and @path='" +
+            frame.getAttribute("scriptPath") + "']"
+        ) || mdlDbgBreakpoints.queryNode(
+            "//breakpoint[@line='" + bp.line +"' and @path='" +
+            this.getPathFromScriptId(bp.script_id) + "']"
+        );
 
         if (uibp && uibp.getAttribute("enabled") == "true")
             return;
